87f35f5f55a639cc249060262934025f0174df21
Filip Cudny <filip96zx@gmail.com>
2023-09-09 10:57:25 +0200

    Konfiguracja graphql, servera, inicjalizacja bazy

---
+++ src/graphql/schema/resolvers/Query/product.ts
@@ -0,0 +1,14 @@
+import { prisma } from '../../../../db';
+import type { QueryResolvers } from './../../../types.generated';
+export const product: NonNullable<QueryResolvers['product']> = async (
+  _parent,
+  arg,
+  _ctx
+) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: arg.id
+    }
+  });
+  return product;
+};
