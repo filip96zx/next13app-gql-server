0ff43deefd69dd0179dbbe0d2fc93cb9a9b9e02d
Filip Cudny <filip96zx@gmail.com>
2023-10-10 23:47:42 +0200

    Implementacja mutacji obliczania średniej oceny

--- src/graphql/schema/resolvers/Mutation/productCalculateAndUpdateAverageRating.ts
+++ src/graphql/schema/resolvers/Mutation/productCalculateAndUpdateAverageRating.ts
@@ -1,7 +1,12 @@
+import {
+	parseProductToProductWithNotNullableLists,
+	updateProductAverageRating
+} from 'graphql/schema/resolvers/shared/product.utils';
 import type { MutationResolvers } from './../../../types.generated';
-//@ts-ignore
+
 export const productCalculateAndUpdateAverageRating: NonNullable<
 	MutationResolvers['productCalculateAndUpdateAverageRating']
-> = async (_parent, _arg, _ctx) => {
-	/* Implement Mutation.productCalculateAndUpdateAverageRating resolver logic here */
+> = async (_parent, arg, _ctx) => {
+	const product = await updateProductAverageRating(arg.id);
+	return parseProductToProductWithNotNullableLists(product);
 };
