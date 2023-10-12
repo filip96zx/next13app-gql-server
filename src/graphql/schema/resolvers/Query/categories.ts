aa8c565770ba45aca27916678c1dacf1e45401d6
Filip Cudny <filip96zx@gmail.com>
2023-10-12 22:44:59 +0200

    Dodanie filtrowania po slug'u

--- src/graphql/schema/resolvers/Query/categories.ts
+++ src/graphql/schema/resolvers/Query/categories.ts
@@ -2,10 +2,13 @@ import type { QueryResolvers } from 'graphql/types.generated';
 export const categories: NonNullable<
 	QueryResolvers['categories']
 > = async (_parent, arg, ctx) => {
-	const { skip, first } = arg || {};
+	const { skip, first, where } = arg || {};
 	const categories = await ctx.prisma.category.findMany({
 		skip: skip ?? undefined,
-		take: first ?? undefined
+		take: first ?? undefined,
+		where: {
+			slug: where?.slug || undefined
+		}
 	});
 
 	return categories.map((c) => ({
